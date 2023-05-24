b84a71d162845aae860b22d95550cb8c39dbf180
Jace <jacedenny@gmail.com>
2023-05-24 22:28:11 +1000

    Use Frsky and GPS at the same time

--- Boat_Control.ino
+++ Boat_Control.ino
@@ -1,34 +1,65 @@
 #include <ArduinoBLE.h>
 #include <TinyGPS++.h>
+#include <Servo.h>
+#include "sbus.h"
+#include "wiring_private.h"
+
+#define GPS_RX 5
+#define GPS_TX 6
+#define MOTOR1 7
+#define MOTOR2 8
+#define MOTOR_TEST false
 
 BLEService boatService("fff0");
 
+// Please don't change
 const int MAXCOORDS = 32;
 
+// Bluetooth setup
 BLEStringCharacteristic debugCharacteristic("45c1eda2-4473-42a3-8143-dc79c30a64bf", BLENotify | BLERead, 100);
 BLECharCharacteristic commandCharacteristic("05c6cc87-7888-4588-b794-92bdf9a29330", BLEWrite);
 BLECharacteristic coordsCharacteristic("3794c841-1b53-4029-aebb-12319386fd28", BLEWrite, 16*MAXCOORDS, true);
 BLECharacteristic telemetryCharacteristic("ccc03716-4f66-4cb8-b6fd-9b2278587add", BLENotify, 100, false);
 
 TinyGPSPlus gps;
+Uart gpsSerial (&sercom0, GPS_RX, GPS_TX, SERCOM_RX_PAD_1, UART_TX_PAD_0);
+void SERCOM0_Handler(){
+    gpsSerial.IrqHandler();
+}
 
-unsigned long prevMillis = 0;
+// Frsky setup
+bfs::SbusRx sbus(&Serial1);
+bfs::SbusData data;
+Servo g_motor1;
+Servo g_motor2;
 
-int numWaypoints = 0;
-int waypointPointer = 0;
-double waypointCoords[10] = {};
+unsigned long g_prevMillis = 0;
 
-byte telemOutput[100];
+byte g_telemOutput[100];
 
-bool homeSet = false;
-double homeLat;
-double homeLng;
+// Waypoint data
+int g_numWaypoints = 0;
+int g_waypointPointer = 0;
+double g_waypointCoords[10] = {};
 
-const unsigned int telemSize = sizeof(double)*4;
-double *p_x   = (double*) (telemOutput);
-double *p_y   = (double*) (telemOutput + 8);
-double *p_lat = (double*) (telemOutput + 16);
-double *p_lng = (double*) (telemOutput + 24);
+// Homing data
+bool g_homeSet = false;
+double g_homeLat;
+double g_homeLng;
+
+// Frsky Control
+const int DEADZONE = 100;
+
+bool g_manualControl = true;
+bool g_frskyZeroSet = false;
+float g_frskyZeroX;
+float g_frskyZeroY;
+
+const unsigned int TELEM_SIZE = sizeof(double)*4;
+double *p_x   = (double*) (g_telemOutput);
+double *p_y   = (double*) (g_telemOutput + 8);
+double *p_lat = (double*) (g_telemOutput + 16);
+double *p_lng = (double*) (g_telemOutput + 24);
 
 void setup() {
     if (!BLE.begin()){
@@ -41,7 +72,12 @@ void setup() {
     }
 
     Serial.begin(9600);
-    Serial1.begin(9600);
+    gpsSerial.begin(9600);
+
+    pinPeripheral(GPS_RX, PIO_SERCOM_ALT);
+    pinPeripheral(GPS_TX, PIO_SERCOM_ALT);
+
+    sbus.Begin();
 
     pinMode(LED_BUILTIN, OUTPUT);
 
@@ -65,13 +101,17 @@ void loop() {
 
         while (central.connected()){
             unsigned long currentMillis = millis();
-            if (currentMillis - prevMillis > 100){
+            if (currentMillis - g_prevMillis > 100){
                 processEvents();
             }
         }
 
         digitalWrite(LED_BUILTIN, LOW);
     }
+    if (g_manualControl){
+        getRCControl();
+    }
+
 }
 
 
@@ -80,7 +120,7 @@ void processCommand(){
     switch(cmd){
         case 'h':
             debugCharacteristic.writeValue("Home reset");
-            homeSet = false;
+            g_homeSet = false;
             break;
         default :
             debugCharacteristic.writeValue("Something else");
@@ -89,48 +129,46 @@ void processCommand(){
 }
 
 void processCoords(){
-    numWaypoints = MAXCOORDS;
+    g_numWaypoints = MAXCOORDS;
     for (int i = 0;i < MAXCOORDS*2; i++){
         double* pCoords = (double*) coordsCharacteristic.value();
-        waypointPointer = 0;
+        g_waypointPointer = 0;
         if (abs(pCoords[i]) < 0.01){
             // Check odd coords sent
             if (i % 2 == 1){
                 debugCharacteristic.writeValue("Invalid coord count");
             } else {
-                numWaypoints = i / 2;
+                g_numWaypoints = i / 2;
             }
             break; // We are done reading
         }else{
             // Add waypoint
-            waypointCoords[i] = pCoords[i];
+            g_waypointCoords[i] = pCoords[i];
         }
     }
 
     String output = "Coords written: ";
-    output += numWaypoints;
+    output += g_numWaypoints;
     debugCharacteristic.writeValue(output);
 }
 
 void processGPS(){
-    Serial.print("Valid ");
-    Serial.println(gps.location.isValid());
     if (gps.location.isValid()){
-        if (homeSet == false){
-            homeLat = gps.location.lat();
-            homeLng = gps.location.lng();
-            homeSet = true;
+        if (g_homeSet == false){
+            g_homeLat = gps.location.lat();
+            g_homeLng = gps.location.lng();
+            g_homeSet = true;
         } else {
             double lat = gps.location.lat();
             double lng = gps.location.lng();
             double dist = TinyGPSPlus::distanceBetween(
-                homeLat,
-                homeLng,
+                g_homeLat,
+                g_homeLng,
                 lat,
                 lng);
             double angle = TinyGPSPlus::courseTo(
-                homeLat,
-                homeLng,
+                g_homeLat,
+                g_homeLng,
                 lat,
                 lng);
             double x = dist*cos(angle*DEG_TO_RAD);
@@ -145,6 +183,69 @@ void processGPS(){
     }
 }
 
+void getRCControl(){
+    int x;
+    int y;
+    int m;
+    int dir1;
+    int dir2;
+
+    if (sbus.Read()){
+        data = sbus.data();
+        if (g_frskyZeroSet){
+            x = data.ch[1] - g_frskyZeroX;
+            y = data.ch[2] - g_frskyZeroY;
+            m = data.ch[4];
+            dir1 = data.ch[5];
+            dir2 = data.ch[6];
+        } else {
+            g_frskyZeroX = data.ch[1];
+            g_frskyZeroY = data.ch[2];
+            g_frskyZeroSet = true;
+        }
+    }
+
+    // Deadzone
+    if (abs(x) < DEADZONE){
+        x = 0;
+    }
+    if (abs(y) < DEADZONE){
+        y = 0;
+    }
+
+    long power1 = y + x;
+    long power2 = y - x;
+
+    if(dir1 < 300){
+        power1*=-1;
+    }
+    if(dir2 < 300){
+        power2*=-1;
+    }
+
+    power1 = power1*m/4000 + 1500;
+    power2 = power2*m/4000 + 1500;
+
+    power1 = constrain(power1, 1100, 1900);
+    power2 = constrain(power2, 1100, 1900);
+
+    #if(MOTOR_TEST)
+    Serial.print("Motor1:");
+    Serial.print(power1);
+    Serial.print(",Motor2:");
+    Serial.print(power2);
+
+    Serial.print(",Dir1:");
+    Serial.print(dir1);
+    Serial.print(",Dir2:");
+    Serial.println(dir2);
+    delay(100);
+    #else
+    g_motor1.writeMicroseconds(power1);
+    g_motor2.writeMicroseconds(power2);
+    #endif
+}
+
 void processEvents(){
     // Only runs if connected to device
 
@@ -155,10 +256,10 @@ void processEvents(){
         processCoords();
     }
 
-    while (Serial1.available() > 0){
-        if (gps.encode(Serial1.read())){
+    while (gpsSerial.available() > 0){
+        if (gps.encode(gpsSerial.read())){
             processGPS();
         }
     }
-    telemetryCharacteristic.writeValue((byte*) telemOutput, telemSize);
+    telemetryCharacteristic.writeValue((byte*) g_telemOutput, TELEM_SIZE);
 }
